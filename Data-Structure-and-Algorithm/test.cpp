ed512c391387ee1651c19b3c4ea9c0ec0f69b9e6
VorngViseth <vorngviseth077@gmail.com>
2025-09-30 14:51:23 +0700

    setting

--- Data-Structure-and-Algorithm/test.cpp
+++ Data-Structure-and-Algorithm/test.cpp
@@ -11,16 +11,21 @@ int main() {
 
     // cout << sum << endl;
 
-    vector <int> vecNum = {1,2,3,4,5};
-    cout << vecNum.at(4) << endl;
+    // vector <int> vecNum = {1,2,3,4,5};
+    // cout << vecNum.at(4) << endl;
 
-    vecNum.pop_back();
-    vecNum.push_back(6);
+    // vecNum.pop_back();
+    // vecNum.push_back(6);
     
 
-    for(int i: vecNum) {
-        cout<<i<<endl;
-    }
+    // for(int i: vecNum) {
+    //     cout<<i<<endl;
+    // }
+
+    cout << "Size of bool is : " << sizeof(bool) << endl;
+    cout << "Size of Char is : " << sizeof(char) << endl;
+    cout << "Size of Float is : " << sizeof(float) << endl;
+    cout << "Size of Int is : " << sizeof(int) << endl;
 
     return 0;
 }   
