0a3119e5477059c5638fa125b81e6d7e315ee1c4
LancD <Vorngviseth077@gmail.com>
2025-10-14 22:38:39 +0700

    lab week 2

--- Data-Structure-and-Algorithm/week2/class.cpp
+++ Data-Structure-and-Algorithm/week2/class.cpp
@@ -8,14 +8,17 @@ int main() {
 
     LinkedList list;
 
-    list.insertFront(19);
-    list.insertFront(19);
-    list.insertFront(19);
-    list.insertFront(19);
-    list.insertBack(12);
+    list.insertFront(3);
+    list.insertFront(1);
+    list.insertBack(4);
+    list.insertMiddle(2);
+    // list.deleteMiddle();
+    list.swapTwoNodes(0, 1);
 
     list.print();
     cout << "the size of list is : " << list.getSize() << endl;
+    cout << "the tail of the list is : " << list.getTail() << endl;
+    cout << "the head of the list is : " << list.getHead() << endl;
 
     return 0;
 }
--- Data-Structure-and-Algorithm/week2/class.hpp
+++ Data-Structure-and-Algorithm/week2/class.hpp
@@ -6,71 +6,197 @@ using namespace std;
 struct Node
 {
     int value;
-    Node* next;
+    Node *next;
 };
 
-class LinkedList{
-    private:
-        Node *head, *tail, *current;
-        int size;
-    
-    public:
+class LinkedList
+{
+private:
+    Node *head, *tail, *current;
+    int size;
 
-        void _travers() {
-            current = head;
-            while(current->next) current = current->next;
+public:
+    LinkedList()
+    {
+        head = nullptr;
+        size = 0;
+    }
+
+    int getSize() { return size; }
+
+    int getTail() { return tail->value; }
+
+    int getHead() { return head->value; }
+
+    void _travers()
+    {
+        current = head;
+        while (current->next)
+            current = current->next;
+    }
+
+    void _traverseTill(int pos)
+    {
+        current = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            current = current->next;
         }
+    }
 
-        int getSize() {return size;}
-
-        LinkedList() {
-            head = nullptr;
-            size = 0;
-        }
-
-        void insertFront(int value){
-            Node* newNode = new Node{value, nullptr};
-            newNode->next = head;
-            head = newNode;
-            size++ ;
-        }
-
-        void insertBack(int value) {
-            if(size == 0) {
-                insertFront(value);
-                return;
-            }
-
-            _travers();
-            Node* newNode = new Node{value, nullptr};
-            current->next = newNode;
-            size++;
+    void insertFront(int value)
+    {
+        Node *newNode = new Node{value, nullptr};
+        newNode->next = head;
+        head = newNode;
+        if (size == 0)
             tail = newNode;
-            
+        size++;
+    }
+
+    void insertBack(int value)
+    {
+        if (size == 0)
+        {
+            insertFront(value);
+            return;
         }
 
-        void insertAt(int pos, int value){
-            if(pos == 0) {
-                insertFront(value);
-                return;
-            }
+        Node *newNode = new Node{value, nullptr};
+        tail->next = newNode;
+        size++;
+        tail = newNode;
+    }
 
-            if(pos >= size) {
-                insertBack(value);
-                return;
-            }
-
-            
+    void insertAt(int pos, int value)
+    {
+        if (pos == 0)
+        {
+            insertFront(value);
+            return;
         }
 
-        void print(){
-            current = head;
-            while(current->next) {
-                cout << current->value << "->";
-                current = current->next;
-            }
-
-            cout << current->value << endl;
+        if (pos >= size)
+        {
+            insertBack(value);
+            return;
         }
 
+        Node *newNode = new Node{value, nullptr};
+        Node *temp;
+        _traverseTill(pos);
+        temp = current->next;
+        current->next = newNode;
+        newNode->next = temp;
+        size++;
+    }
+
+    void insertMiddle(int value)
+    {
+        _traverseTill(size / 2);
+        Node *newNode = new Node{value, nullptr};
+        Node *temp;
+        temp = current->next;
+        current->next = newNode;
+        newNode->next = temp;
+        size++;
+    }
+
+    void deleteFront()
+    {
+        if (head == nullptr)
+        {
+            cout << "the list is empty!" << endl;
+            return;
+        }
+
+        Node *temp;
+        temp = head;
+        head = head->next;
+        delete temp;
+        size--;
+        if (head == nullptr)
+            tail = nullptr;
+    }
+
+    void deleteEnd()
+    {
+        if (head == 0)
+        {
+            cout << "The list is empty" << endl;
+            return;
+        }
+
+        Node *temp;
+        temp = tail;
+        _traverseTill(size);
+        tail = current;
+        delete temp;
+    }
+
+    void deleteMiddle()
+    {
+        if (head == nullptr)
+        {
+            cout << "the list is empty" << endl;
+            return;
+        }
+
+        if (size == 1)
+        {
+            deleteFront();
+            return;
+        }
+
+        Node *temp;
+        _traverseTill(size / 2);
+        temp = current->next;
+        current->next = temp->next;
+        if (temp == tail)
+            tail = current;
+        delete temp;
+        size--;
+    }
+
+    void print()
+    {
+        current = head;
+        while (current->next)
+        {
+            cout << current->value << "->";
+            current = current->next;
+        }
+
+        cout << current->value << endl;
+    }
+
+    void swapTwoNodes(int posFisrtNode, int posSecNode)
+    {
+        if (head == nullptr || size == 1)
+        {
+            cout << "The list is empty/not enough" << endl;
+            return;
+        }
+
+        if (posFisrtNode < 0 || posSecNode >= size)
+        {
+            cout << "Error" << endl;
+            return;
+        }
+
+        Node *prevFrist;
+        Node *firstNode;
+        _traverseTill(posFisrtNode);
+        prevFrist = current;
+        firstNode = current->next;
+
+        Node *prevSec;
+        Node *secNode;
+        _traverseTill(posSecNode);
+        prevSec = current;
+        secNode = current->next;
+
+        if(prevFrist    )
+
+    }
 };
