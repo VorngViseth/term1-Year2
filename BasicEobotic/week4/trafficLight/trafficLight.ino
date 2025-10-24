f0a507144c94ca74eaf45ce5a891a932b44161fa
LancD <Vorngviseth077@gmail.com>
2025-10-24 21:05:47 +0700

    feat : traffic 4 ways

--- BasicEobotic/week4/trafficLight/trafficLight.ino
+++ BasicEobotic/week4/trafficLight/trafficLight.ino
@@ -6,6 +6,14 @@
 #define greenLed2 14
 #define yellowLed2 15
 
+#define redLed3 42
+#define greenLed3 41
+#define yellowLed3 40
+
+#define redLed4 37
+#define greenLed4 39
+#define yellowLed4 38
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(redLed1, OUTPUT);
@@ -16,13 +24,27 @@ void setup() {
   pinMode(greenLed2, OUTPUT);
   pinMode(yellowLed2, OUTPUT);
 
+  pinMode(redLed3, OUTPUT);
+  pinMode(greenLed3, OUTPUT);
+  pinMode(yellowLed3, OUTPUT);  
+
+  pinMode(redLed4, OUTPUT);
+  pinMode(greenLed4, OUTPUT);
+  pinMode(yellowLed4, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  trafficStop(greenLed3, yellowLed3, redLed3);
+  trafficStop(greenLed4, yellowLed4, redLed4);
   traffic(greenLed1, yellowLed1, redLed1, greenLed2, yellowLed2, redLed2);
   traffic(greenLed2, yellowLed2, redLed2, greenLed1, yellowLed1, redLed1);
+
+  trafficStop(greenLed1, yellowLed1, redLed1);
+  trafficStop(greenLed2, yellowLed2, redLed2);
+  traffic(greenLed3, yellowLed3, redLed3, greenLed4, yellowLed4, redLed4);
+  traffic(greenLed4, yellowLed4, redLed4, greenLed3, yellowLed3, redLed3);
 
 
 }
