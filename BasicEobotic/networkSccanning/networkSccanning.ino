9bf13f7a9a523653ea02cae3c74080a46127642a
LancD <Vorngviseth077@gmail.com>
2025-10-28 21:59:19 +0700

    fix : wifi scan

--- BasicEobotic/networkSccanning/networkSccanning.ino
+++ BasicEobotic/networkSccanning/networkSccanning.ino
@@ -1,16 +1,16 @@
 #include "WiFi.h" 
 
-#define redLed 4
-#define yellowLed 5
-#define greenLed 13
+// #define redLed 4
+// #define yellowLed 5
+// #define greenLed 13
   
 void setup() {
   // put your setup code here, to run once:
-  pinMode(redLed, OUTPUT);
-  pinMode(yellowLed, OUTPUT);
-  pinMode(greenLed, OUTPUT);
+  // pinMode(redLed, OUTPUT);
+  // pinMode(yellowLed, OUTPUT);
+  // pinMode(greenLed, OUTPUT);
 
-  WiFi.begin(115200);
+  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(1000);
@@ -20,14 +20,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Scanning available networks..");
-  digitalWrite(redLed, 0);
-  digitalWrite(greenLed, 0);
-  digitalWrite(yellowLed, 1);
+  // digitalWrite(redLed, 0);
+  // digitalWrite(greenLed, 0);
+  // digitalWrite(yellowLed, 1);
   
   int wifi = WiFi.scanNetworks();
   if(wifi != 0) {
     Serial.print(wifi);
-    Serial.println(" network(s) found : ")
+    Serial.println(" network(s) found : ");
     for(int i = 0; i < wifi; i++) {
       Serial.print("network "); Serial.print(i+1); Serial.print(" : ");
       Serial.print(WiFi.SSID(i));
@@ -38,9 +38,9 @@ void loop() {
   }
   else {
     Serial.println("No available networks");
-    digitalWrite(redLed, 1);
-    digitalWrite(greenLed, 0);
-    digitalWrite(yellowLed, 0);
+  //   digitalWrite(redLed, 1);
+  //   digitalWrite(greenLed, 0);
+  //   digitalWrite(yellowLed, 0);
   }
 
   delay(5000);
